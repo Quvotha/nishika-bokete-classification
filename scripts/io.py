842316eb59ae2c8967057c957e7be15d8eda04d3
Quvotha <nndropout1+github@gmail.com>
2022-09-16 02:29:29 +0900

    [add]Copy and paste `read_jpg` from tutorial

---
+++ scripts/io.py
@@ -0,0 +1,24 @@
+import torch
+import torchvision
+
+
+def read_jpg(path: str):
+    """jpg 画像の読み込み。
+
+    Tutorial のコードから丸パクりしたもの。
+
+    Parameters
+    ----------
+    path : str
+        画像のファイルパス。
+
+    Returns
+    -------
+    image_tensor: torch.Tensor
+        画像のテンソル。
+    """
+    image_tensor = torchvision.io.read_image(path)
+    if image_tensor.shape[0] == 1:
+        # 1channel=白黒画像があるので3channelにconvertしています。
+        image_tensor = image_tensor.expand(3, *image_tensor.shape[1:])
+    return image_tensor
